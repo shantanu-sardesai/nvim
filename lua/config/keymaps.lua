140a8c8331f021524725d3c05a7e1f25e7945bf4
Shantanu Sardesai <shantanu.s.sardesai@gmail.com>
2024-11-01 15:52:56 +0530

    feat: modify keybindings & default behaviours

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- center the cursor on the half page jumps.
+map("n", "<C-d>", "<C-d>zz")
+
+-- center the cursor on the search object within the screen.
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- keep the copied paste register intact while overwriting, handy when we want to paste the same thing in multiple places.
+map("x", "<leader>p", '"_dP')
+
+-- copy to system clipboard.
+map("n", "<leader>y", '"+y')
+map("v", "<leader>y", '"+y')
+map("n", "<leader>Y", '"+Y')
+
+-- delete without overwritin the paste register.
+map("n", "<leader>d", '"_d')
+map("v", "<leader>d", '"_d')
